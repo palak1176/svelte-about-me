537c4d6d55be14d8493d68304117ade9d244cfe3
palak1176 <sspalak@gmail.com>
2025-07-17 22:23:48 +0530

    Customized about me page

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,41 @@
 <script>
   let name = "Palak Mor"; // Change this to your name
   let favoriteNumber = 16; // Change this to your favorite number
-  let bio = "I am a web developer with a passion for building cool things!";
+  let bio = "I am from Cumming, Georgia. I am currently a data collection and structuring intern at Syensqo, a multinational material science company. I am super excited to start at Georgia Tech to pursue a major in industrial engineering with a concentration in data analytics and science this fall!";
   let hobbies = ["Diamond Painting", "Listening to Podcasts", "Spending Time with Friends"]; // List some hobbies
     import ImageCard from "./lib/ImageCard.svelte";
   
   let cardData = [
     {
       image:
-        "https://media.istockphoto.com/id/185590965/photo/yellow-rubber-duck-for-bath-time.jpg?s=612x612&w=0&k=20&c=QoT-O5jbOugCgdQhLat15c0L9jCmRrSTiO9U50W_eQc=",
+        "https://mybobbin.com/storage/photo/resized/xy_1300x1000/c/25jieewin008xxe_a9660fb.jpg",
       title: "Diamond Painting",
       description:
-        "I love ",
+        "I love diamond painting! My last painting was a 22 x 28 inch landscape of a balcony overlooking the sea.",
     },
     {
-      image: "https://media.istockphoto.com/id/1460007178/photo/library-books-on-table-and-background-for-studying-learning-and-research-in-education-school.jpg?s=612x612&w=is&k=20&c=PvjSrEGbw3G64q74ccIQaJ9nEj9AzPwjvRo1cWFA3UU=",
+      image: "https://i.scdn.co/image/ab6765630000ba8a4a2f177477ecd05b6f5be8e0",
       title: "Listening to Podcasts",
-      description: "My favorite podcast so far is 'My Favorite Murder by Karen Kilgariff and Georgia Hardstark'",
+      description: "I started listening to my favorite podcast, My Favorite Murder by Karen Kilgariff and Georgia Hardstark, 5 years ago.",
     },
     {
-      image: "https://media.istockphoto.com/id/1440399157/photo/international-airport-terminal-asian-beautiful-woman-with-luggage-and-walking-in-airport.jpg?s=612x612&w=0&k=20&c=RqmI0KG_pnEb_GmWLC6pY5MoV75JBt1nU1L9dStkJys=",
+      image: "https://i.kickstarter.com/assets/029/996/500/ed20ba38456d6d5caf29e150dbef0522_original.jpg?anim=false&fit=cover&gravity=auto&height=576&origin=ugc&q=92&v=1596039396&width=1024&sig=OD7G8aQ5YMrcc0a31FGgfz3xh2YCnlDw1es9dhwzVDI%3D",
       title: "Spending Time with Friends",
       description:
-        "I enjoy exploring new cultures and learning from the world around me.",
+        "I love hanging out with my friends and taking pictures on our digicams or playing card games like Cabo.",
     },
   ];
   
   </script>
 
 <style>
+  .html {
+    background-color: rgb(181, 203, 243),
+  }
+  
   h1 {
     color: #2c3e50;
-    font-family: Arial, sans-serif;
+    font-family: 'Trebuchet MS';
   }
   .container {
     max-width: 800px;
@@ -53,6 +57,7 @@
   }
 </style>
 
+<div class="html">
 <div class="container">
   <h1>About Me</h1>
   <p>Hello, my name is <strong>{name}</strong>!</p>
@@ -70,5 +75,6 @@
     {#each cardData as { image, title, description }}
       <ImageCard {image} {title} {description} />
     {/each}
+  </div>
   </div>
 </div>
